d254018cc52bededf10abc5831f622e3fa4ed6c5
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-03-21 15:00:30 -0400

    removed additional extraneous code from displayEmployee to collectEmplyee function

--- script.js
+++ script.js
@@ -59,40 +59,7 @@ const getRandomEmployee = function (employeesArray) {
   Do not modify any of the code below this line:
 */
 
-// Display employee data in an HTML table
-const displayEmployees = function (employeesArray) {
-  // Get the employee table
-  const employeeTable = document.querySelector('#employee-table');
 
-  // Clear the employee table
-  employeeTable.innerHTML = '';
-
-  // Loop through the employee data and create a row for each employee
-  for (let i = 0; i < employeesArray.length; i++) {
-    const currentEmployee = employeesArray[i];
-
-    const newTableRow = document.createElement("tr");
-
-    const firstNameCell = document.createElement("th");
-    firstNameCell.textContent = currentEmployee.firstName;
-    newTableRow.append(firstNameCell);
-
-    const lastNameCell = document.createElement("td");
-    lastNameCell.textContent = currentEmployee.lastName;
-    newTableRow.append(lastNameCell);
-
-    const salaryCell = document.createElement("td");
-    // Format the salary as currency
-    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US", {
-      style: "currency",
-      currency: "USD"
-    });
-
-    newTableRow.append(salaryCell);
-
-    employeeTable.append(newTableRow);
-  }
-}
 
 const trackEmployeeData = function () {
   const employees = collectEmployees();
